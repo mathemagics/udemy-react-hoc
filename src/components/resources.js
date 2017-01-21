a916c38cd9d6314431b2a8b9f1fdb3e3e8d1fe82
Paul WB <higgsbison@gmail.com>
2017-01-21 14:23:33 -0800

    create resources component and add children to app

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+    return (
+      <div>
+        Super Secret Recipe
+        <ul>
+          <li>1 cup sugar</li>
+          <li>1 cup flour</li>
+          <li>1 tsp salt</li>
+        </ul>
+      </div>
+    )
+};
+ 
